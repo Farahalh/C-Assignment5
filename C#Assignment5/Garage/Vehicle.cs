6de33fcb46562dae280fbb961cdde9574379f7f8
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-10-11 00:03:16 +0200

    Creating Vehicle class....
    with base vehicle and its subclasses

--- C#Assignment5/Garage/Vehicle.cs
+++ C#Assignment5/Garage/Vehicle.cs
@@ -1,12 +1,89 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace C_Assignment5.Garage
 {
-    internal class Vehicle
+    public class Vehicle
     {
+        // Common properties and methods for Vehicles
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public int NumberOfWheels { get; set; }
+        public int NumberOfEngines { get; set; }
+        public string FuelType { get; set; }
+
+        public Vehicle(string registrationNumber, string color, int numberOfWheels, int numberOfEngines, string fuelType)
+        {
+            RegistrationNumber = registrationNumber;
+            Color = color;
+            NumberOfWheels = numberOfWheels;
+            NumberOfEngines = numberOfEngines;
+            FuelType = fuelType;
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        // Additional properties and methods specific to cars
+        public int NumberOfDoors { get; set; }
+
+        public Car(string registrationNumber, string color, int numberOfWheels, int numberOfEngines, string fuelType, int numberOfDoors)
+        : base(registrationNumber, color, numberOfWheels, numberOfEngines, fuelType)
+        {
+            NumberOfDoors = numberOfDoors;
+        }
+    }
+
+    public class Airplane : Vehicle
+    {
+        // Additional properties and methods specific to airplanes
+        public int MaximumAltitude { get; set; }
+
+        public Airplane(string registrationNumber, string color, int numberOfWheels, int numberOfEngines, string fuelType, int maximumAltitude)
+        : base(registrationNumber, color, numberOfWheels, numberOfEngines, fuelType)
+        {
+            MaximumAltitude = maximumAltitude;
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        // Additional properties and methods specific to motorcycles
+        public int CylinderVolume { get; set; }
+
+        public Motorcycle(string registrationNumber, string color, int numberOfWheels, int numberOfEngines, string fuelType, int cylinderVolume)
+        : base(registrationNumber, color, numberOfWheels, numberOfEngines, fuelType)
+        {
+            CylinderVolume = cylinderVolume;
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        // Additional properties and methods specific to buses
+        public int NumberOfSeats { get; set; }
+
+        public Bus(string registrationNumber, string color, int numberOfWheels, int numberOfEngines, string fuelType, int numberOfSeats) 
+            : base(registrationNumber, color, numberOfWheels, numberOfEngines, fuelType)
+        {
+            NumberOfSeats = numberOfSeats;
+        }
+    }
+
+    public class Boat : Vehicle
+    {
+        // Additional properties and methods specific to boats
+        public int Length { get; set; }
+
+        public Boat(string registrationNumber, string color, int numberOfWheels, int numberOfEngines, string fuelType, int length) 
+            : base(registrationNumber, color, numberOfWheels, numberOfEngines, fuelType)
+        {
+            Length = length;
+        }
     }
 }
